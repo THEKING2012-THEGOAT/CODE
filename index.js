72786b98d46158308737e6643934cbbeec3cfe51
THEKING2012-THEGOAT <skramadugu@gmail.com>
2025-05-04 10:51:33 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  document.getElementsByTagName("ul")[2].getElementsByTagName("li")[0].innerHTML = "Milk";
+}
+
+document.getElementById("para").innerHTML="Hello!! I am saawanth";
+
+
+document.getElementsByClassName("example")[0].innerHTML = "Hello World!";
+
